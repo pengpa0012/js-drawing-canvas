5fc25c233726145d39b06fdc5c52188a544dee2a
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-19 08:19:10 +0800

    added color picker

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const canvas = document.querySelector("#canvas")
 const brushSizes = document.querySelectorAll(".brush-size")
 const clearBtn = document.querySelector(".clear-btn")
+const colorPicker = document.querySelector(".color-picker")
 
 const ctx = canvas.getContext("2d")
 const rect = canvas.getBoundingClientRect();
 canvas.width = 900
 canvas.height = 700
 let isDrawing = false
+let brushColor = "#000"
 
 canvas.addEventListener("mousemove", e => {
   if (!isDrawing) return
@@ -17,6 +19,7 @@ canvas.addEventListener("mousemove", e => {
 
   ctx.beginPath();
   ctx.arc(x, y, selectedBrushSize, 0, 2 * Math.PI);
+  ctx.fillStyle = brushColor
   ctx.fill();
 })
 
@@ -31,11 +34,17 @@ brushSizes.forEach(size => {
   })
 })
 
+colorPicker.addEventListener("input", e => {
+  const color = e.target.value
+  brushColor = color
+})
+
 clearBtn.addEventListener("click", () => ctx.clearRect(0 ,0, canvas.width, canvas.height))
 
 
 // Todo
-// resizing of brushes
-// Colors
 // Save as image
 // Add pressure sentitivity (Not required)
+
+// To Fix
+// Circle gaps
