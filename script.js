107143de7ad4b154d0530876658853123725f1e4
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-19 09:45:19 +0800

    added save as image

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("#canvas")
 const brushSizes = document.querySelector(".brush-size")
 const clearBtn = document.querySelector(".clear-btn")
+const saveBtn = document.querySelector(".save-btn")
 const colorPicker = document.querySelector(".color-picker")
 
 const ctx = canvas.getContext("2d")
@@ -38,9 +39,18 @@ colorPicker.addEventListener("input", e => {
 
 clearBtn.addEventListener("click", () => ctx.clearRect(0 ,0, canvas.width, canvas.height))
 
+saveBtn.addEventListener("click", () => {
+  const dataURL = canvas.toDataURL("image/png")
+  const link = document.createElement("a")
+  link.href = dataURL;
+  link.download = "canvas_image.png"
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+})
+
 
 // Todo
-// Save as image
 // Add pressure sentitivity (Not required)
 
 // To Fix
