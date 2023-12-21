6de47d69caf4b61e095c9c985b571d3045c45771
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-21 21:33:27 +0800

    added event listener for mobile

--- script.js
+++ script.js
@@ -12,21 +12,58 @@ let isDrawing = false
 let brushColor = "#000"
 let selectedBrushSize = 5
 
-canvas.addEventListener("mousemove", e => {
-  if (!isDrawing) return
+function getPosition(e) {
+  if (e.touches && e.touches.length > 0) {
+    return {
+      x: e.touches[0].pageX - rect.left,
+      y: e.touches[0].pageY - rect.top
+    };
+  } else {
+    return {
+      x: e.pageX - rect.left,
+      y: e.pageY - rect.top
+    };
+  }
+}
 
-  let x = e.clientX - rect.left
-  let y = e.clientY - rect.top
-
+function draw(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, selectedBrushSize, 0, 2 * Math.PI);
-  ctx.fillStyle = brushColor
+  ctx.fillStyle = brushColor;
   ctx.fill();
-})
+}
 
-canvas.addEventListener("mousedown", () => isDrawing = true)
-canvas.addEventListener("mouseup", () => isDrawing = false)
-canvas.addEventListener("mouseout", () => isDrawing = false)
+function onMouseMove(e) {
+  if (!isDrawing) return;
+  let pos = getPosition(e);
+  draw(pos.x, pos.y);
+}
+
+function onMouseDown(e) {
+  isDrawing = true;
+  let pos = getPosition(e);
+  draw(pos.x, pos.y);
+}
+
+function onMouseUp() {
+  isDrawing = false;
+}
+
+function onMouseOut() {
+  isDrawing = false;
+}
+
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("touchmove", onMouseMove);
+
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("touchstart", onMouseDown);
+
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("touchend", onMouseUp);
+
+canvas.addEventListener("mouseout", onMouseOut);
+canvas.addEventListener("touchcancel", onMouseOut);
 
 brushSizes.addEventListener("input", (e) => {
   selectedBrushSize = e.target.value
